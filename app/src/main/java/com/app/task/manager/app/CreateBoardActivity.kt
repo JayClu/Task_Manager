b8a513d5ce2844cb9950833c7c8f0952eabefcf0
Jackie <93630528+JayClu@users.noreply.github.com>
2024-07-08 22:28:35 +0700

    ui detail card and board

---
+++ app/src/main/java/com/app/task/manager/app/CreateBoardActivity.kt
@@ -0,0 +1,175 @@
+package com.example.cmpt362project.activities
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Typeface
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.example.cmpt362project.R
+import com.example.cmpt362project.databinding.ActivityCreateBoardBinding
+import com.example.cmpt362project.firebase.FireStoreHandler
+import com.example.cmpt362project.models.Board
+import com.example.cmpt362project.utils.Constants
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.IOException
+
+class CreateBoardActivity : BaseActivity() {
+
+    private var mSelectedImageFileUri: Uri? = null
+    private lateinit var mUserName: String
+    private var mBoardImageURL: String = ""
+
+    private lateinit var binding: ActivityCreateBoardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCreateBoardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setFonts()
+        setActionBar()
+
+        if (intent.hasExtra(Constants.NAME)) {
+            mUserName = intent.getStringExtra(Constants.NAME).toString()
+        }
+
+        binding.createBoardImage.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                Constants.showImageChooser(this)
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    Constants.READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
+
+        binding.createBoardCreateBtn.setOnClickListener {
+            if (mSelectedImageFileUri != null) {
+                uploadBoardImage()
+            } else {
+                showProgressDialog()
+                createBoard()
+            }
+        }
+
+
+    }
+
+    private fun createBoard() {
+        val assignedUsers: ArrayList<String> = ArrayList()
+        assignedUsers.add(getCurrentUserID())
+
+        var board = Board(
+            binding.createBoardNameEt.text.toString(),
+            mBoardImageURL,
+            mUserName,
+            assignedUsers
+        )
+
+        FireStoreHandler().createBoard(this, board)
+
+    }
+
+    private fun uploadBoardImage() {
+        showProgressDialog()
+
+        val sRef: StorageReference = FirebaseStorage.getInstance()
+            .reference
+            .child(
+                "BOARD_IMAGE" + System.currentTimeMillis()
+                        + "." + Constants.getFileExtension(this, mSelectedImageFileUri)
+            )
+
+        sRef.putFile(mSelectedImageFileUri!!).addOnSuccessListener { taskSnapshot ->
+            Log.e(
+                "Firebase Image URL",
+                taskSnapshot.metadata!!.reference!!.downloadUrl!!.toString()
+            )
+
+            taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener { uri ->
+                Log.e("Downloadable Image URL", uri.toString())
+                mBoardImageURL = uri.toString()
+
+                createBoard()
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
+            hideProgressDialog()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Constants.showImageChooser(this)
+            }
+        } else {
+            Toast.makeText(
+                this, "You just denied permission for storage." +
+                        "You can allow it from the settings.", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //check if the user select a photo from gallery
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == Constants.PICK_IMAGE_REQUEST_CODE
+            && data!!.data != null
+        ) {
+            mSelectedImageFileUri = data.data
+            // set the new photo to profile image
+            try {
+                Glide.with(this)
+                    .load(mSelectedImageFileUri)
+                    .fitCenter()
+                    .placeholder(R.drawable.ic_board_place_holder)
+                    .into(binding.createBoardImage)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun boardCreatedSuccessfully() {
+        hideProgressDialog()
+        setResult(Activity.RESULT_OK)
+        finish()
+    }
+
+    private fun setFonts() {
+        val regularFont: Typeface = Typeface.createFromAsset(assets, "Raleway-Regular.ttf")
+        binding.createBoardNameEt.typeface = regularFont
+        binding.createBoardCreateBtn.typeface = regularFont
+    }
+
+    private fun setActionBar() {
+        setSupportActionBar(binding.toolbarCreateBoardActivity)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_back_white)
+            actionBar.title = "Create Board"
+        }
+        binding.toolbarCreateBoardActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+}
