37c5c5cc227d88aae328ae359b09fcfd43dc18f9
JayClu <93630528+JayClu@users.noreply.github.com>
2024-07-16 23:39:54 +0700

    fix permission read storage

--- app/src/main/java/com/app/task/manager/ui/board/CreateBoardActivity.kt
+++ app/src/main/java/com/app/task/manager/ui/board/CreateBoardActivity.kt
@@ -5,10 +5,13 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.app.task.manager.R
@@ -45,20 +48,44 @@ class CreateBoardActivity : BaseActivity<ActivityCreateBoardBinding>() {
         }
     }
 
+    private fun checkPermissionAndPickImageReadExternal() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Constants.showImageChooser(this)
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun checkPermissionAndPickImageMediaImage() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_MEDIA_IMAGES
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Constants.showImageChooser(this)
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                Constants.showImageChooser(this)
+            }
+        }
+
     private fun initListener() {
         binding.createBoardImage.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                Constants.showImageChooser(this)
+            if (Build.VERSION.SDK_INT >= 33) {
+                checkPermissionAndPickImageMediaImage()
             } else {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                    Constants.READ_STORAGE_PERMISSION_CODE
-                )
+                checkPermissionAndPickImageReadExternal()
             }
         }
 
@@ -89,7 +116,6 @@ class CreateBoardActivity : BaseActivity<ActivityCreateBoardBinding>() {
         }, actionFailure = {
             hideProgressDialog()
         })
-
     }
 
     private fun uploadBoardImage() {
